6a180929ba94f9b04ccc971820da9b32b38fcfd8
mpmumau <mpmumau@gmail.com>
2017-05-09 01:15:53 -0400

    Fix

--- inc/movements.h
+++ inc/movements.h
@@ -15,14 +15,14 @@ struct servo_mvmt {
 };
 
 struct servo_mvmt mvmt_walk_a[SERVOS_NUM] = {
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
-    { .start_pos = -0.2f, .end_pos = 0.2f, .delay = 0.0f },
+    { -0.2f, 0.2f, 0.0f },
+    { -0.2f, 0.2f, 0.0f },
+    { -0.2f, 0.2f, 0.0f },
+    { -0.2f, 0.2f, 0.0f },
+    { -0.2f, 0.2f, 0.0f },
+    { -0.2f, 0.2f, 0.0f },
+    { -0.2f, 0.2f, 0.0f },    
+    { -0.2f, 0.2f, 0.0f }
 };
 
 #endif
