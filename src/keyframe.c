1f2cea28bf94b4e61b883c54b80dadbddf086ad1
mpmumau <mpmumau@gmail.com>
2017-05-11 00:38:21 -0400

    Alt syntax

--- src/keyframe.c
+++ src/keyframe.c
@@ -69,8 +69,10 @@ void keyfr_transition(Keyframe **keyfrm, Keyframe *target)
 {
     Keyframe *tmp = malloc(sizeof(Keyframe) * SERVOS_NUM);
 
-    for (int i = 0; i < SERVOS_NUM; i++)
-        tmp[i] = (Keyframe) { robot_getservo(i), target[i]->start_pos, 0.0f };
+    for (int i = 0; i < SERVOS_NUM; i++) {
+        tmp[i] = (Keyframe) { robot_getservo(i), target->start_pos, 0.0f };
+        target++;
+    }
 
     *keyfrm = tmp;
 }
