20b09aed92883c7057703920b3954514a03ad87f
mpmumau <mpmumau@gmail.com>
2017-05-12 11:09:28 -0400

    Fix

--- src/keyframe_handler.c
+++ src/keyframe_handler.c
@@ -143,11 +143,11 @@ static void *keyhandler_main(void *arg)
         {
             next = 0.0f;
 
-            free(servo_pos);
-            free(last_keyfr);             
+            if (last_keyfr)
+                free(last_keyfr);             
 
-            servo_pos = NULL;
-            last_keyfr = NULL;   
+            if (last_keyfr->servo_pos)
+                free(servo_pos);
 
             last_keyfr = (Keyframe *) list_pop(&keyframes);
         }
