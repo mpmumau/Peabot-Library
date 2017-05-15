9f414898ff2ae652160878ced938d4c6d747feed
mpmumau <mpmumau@gmail.com>
2017-05-15 01:24:42 -0400

    Removed negative modifier for knee movementS

--- src/keyframe_factory.c
+++ src/keyframe_factory.c
@@ -101,7 +101,7 @@ Keyframe *keyfactory_walk(void *data, bool reverse)
     float *duration = (float *) data;
     float mod = reverse ? -1.0f : 1.0f;
 
-    float knee_delta = 0.4f * mod;
+    float knee_delta = 0.4f;
     float hip_delta = 0.6f * mod;
 
     float knee_pad_a = 0.60f;
