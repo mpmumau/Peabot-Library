8ef4bf6963bafe86e31a44b9b6898f31dcdbe0d3
mpmumau <mpmumau@gmail.com>
2017-05-14 23:58:20 -0400

    Testing hips again

--- src/keyframe_factory.c
+++ src/keyframe_factory.c
@@ -106,48 +106,14 @@ Keyframe *keyfactory_walk(void *data, bool reverse)
 
     int tmp_ease;
 
-    for (int i = 0; i < SERVOS_NUM; i++)
-    {
-        if (i == FRONT_LEFT_HIP || i == BACK_LEFT_HIP)
-        {
-            tmp_ease = reverse ? EASE_CIRC_OUT : EASE_CIRC_IN; 
-            servo_pos[i] = (ServoPos) { tmp_ease, hip_delta, hip_delta * -1.0f, 0.0f, 0.0f };
-            continue;
-        }
-
-        // if (i == FRONT_RIGHT_HIP || i == BACK_RIGHT_HIP)
-        // {
-        //     servo_pos[i] = (ServoPos) { EASE_CIRC_IN, hip_delta * -1.0f, hip_delta, 0.0f, 0.0f };
-        //     continue;
-        // }
-   
-        // // if (!reverse) 
-        // // {
-        // //     if (i == BACK_LEFT_KNEE || i == FRONT_RIGHT_KNEE)
-        // //     {
-        // //         servo_pos[i] = (ServoPos) { EASE_LINEAR, knee_delta * -1.0f, knee_delta * -1.0f, 0.9f, 0.0f };
-        // //         continue;
-        // //     }
-
-        // //     if (i == BACK_RIGHT_KNEE || i == FRONT_LEFT_KNEE)
-        // //     {
-        // //         servo_pos[i] = (ServoPos) { EASE_LINEAR, knee_delta, knee_delta , 0.0f, 0.0f };
-        // //     }
-        // // }
-        // // else
-        // // {
-        // //     if (i == BACK_RIGHT_KNEE || i == FRONT_LEFT_KNEE)
-        // //     {
-        // //         servo_pos[i] = (ServoPos) { EASE_LINEAR, knee_delta * -1.0f, knee_delta, 0.9f, 0.0f };
-        // //         continue;
-        // //     }
-
-        // //     if (i == BACK_LEFT_KNEE || i == FRONT_RIGHT_KNEE)
-        // //     {
-        // //         servo_pos[i] = (ServoPos) { EASE_LINEAR, knee_delta, knee_delta, 0.0f, 0.0f };
-        // //     }            
-        // // }
-    }
+    // servos_pos[0] = (ServoPos) { EASE_CIRC_IN, -hip_delta, hip_delta, 0.0f, 0.0f };
+    servos_pos[1] = (ServoPos) { EASE_CIRC_IN, -hip_delta, hip_delta, 0.0f, 0.0f };
+    servos_pos[2] = (ServoPos) { EASE_CIRC_OUT, -hip_delta, hip_delta, 0.0f, 0.0f };
+    // servos_pos[3] = (ServoPos) { CircularEaseIn, hip_delta, -hip_delta, 0.0f, 0.0f };
+    // servos_pos[4] = (ServoPos) { CircularEaseIn, hip_delta, -hip_delta, 0.0f, 0.0f };
+    servos_pos[5] = (ServoPos) { EASE_CIRC_OUT, hip_delta, -hip_delta, 0.0f, 0.0f };
+    // servos_pos[6] = (ServoPos) { CircularEaseIn, hip_delta, -hip_delta, 0.0f, 0.0f };
+    servos_pos[7] = (ServoPos) { EASE_CIRC_IN, hip_delta, -hip_delta, 0.0f, 0.0f };
 
     Keyframe *keyfr = malloc(sizeof(Keyframe));
     if (!keyfr)
