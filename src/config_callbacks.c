7ac3bc260c24a476b85969fc9a6bc3d1b1db877d
mpmumau <mpmumau@gmail.com>
2017-05-17 19:29:53 -0400

    Fixed data object references in config callbacks

--- src/config_callbacks.c
+++ src/config_callbacks.c
@@ -28,111 +28,127 @@ void configset_log_filename(Config *config, void *data)
 
 void configset_log_stdin(Config *config, void *data)
 {
-    config->log_stdin = (bool) *data;
+    bool *data_p = (bool *) data;
+    config->log_stdin = *data_p;
     return;
 }
 
 void configset_log_prompt_commands(Config *config, void *data)
 {
-    config->log_prompt_commands = (bool) *data;
+    bool *data_p = (bool *) data;
+    config->log_prompt_commands = *data_p;
 }
 
 void configset_log_event_add(Config *config, void *data)
 {
-    config->log_event_add = (bool) *data;
+    bool *data_p = (bool *) data;
+    config->log_event_add = (bool) *data_p;
     return;
 }
 
 void configset_log_event_callbacks(Config *config, void *data)
 {
-    config->log_event_callbacks = (bool) *data;
+    bool *data_p = (bool *) data;
+    config->log_event_callbacks = *data_p;
     return;
 }
 
 void configset_log_keyframes(Config *config, void *data)
 {
-    config->log_keyframes = (bool) *data;
+    bool *data_p = (bool *) data;
+    config->log_keyframes = *data_p;
     return;
 }
 
 void configset_pca_9685_pin_base(Config *config, void *data) 
 {
-    config->pca_9685_pin_base = (int) *data;
+    int *data_p = (int *) data;
+    config->pca_9685_pin_base = *data_p;
     return;
 }
 
 void configset_pca_9685_max_pwm(Config *config, void *data)
 {
-    config->pca_9685_max_pwm = (int) *data;
+    int *data_p = (int *) data;
+    config->pca_9685_max_pwm = *data_p;
     return;
 }
 
 void configset_pca_9685_hertz(Config *config, void *data)
 {
-    config->pca_9685_hertz = (int) *data;
+    int *data_p = (int *) data;
+    config->pca_9685_hertz = *data_p;
     return;
 }
 
 void configset_servos_num(Config *config, void *data)
 {
-    config->servos_num = (int) *data;
+    int *data_p = (int *) data;
+    config->servos_num = *data_p;
     return;
 }
 
 void configset_robot_tick(Config *config, void *data)
 {
-    config->robot_tick = (float) *data;
+    float *data_p = (float *) data;
+    config->robot_tick = *data_p;
     return;
 }
 
 void configset_transitions_enable(Config *config, void *data)
 {
-    config->transitions_enable = (bool) *data;
+    bool *data_p = (bool *) data;
+    config->transitions_enable = *data_p;
     return;
 }
 
 void configset_transition_time(Config *config, void *data)
 {
-    config->transition_time = (float) *data;
+    float *data_p = (float *) data;
+    config->transition_time = *data_p;
     return;
 }
 
 void configset_walk_hip_delta(Config *config, void *data)
 {
-    config->walk_hip_delta = (float) *data;
+    float *data_p = (float *) data;
+    config->walk_hip_delta = *data_p;
     return;
 }
 
 void configset_walk_knee_delta(Config *config, void *data)
 {
-    config->walk_knee_delta = (float) *data;
+    float *data_p = (float *) data;
+    config->walk_knee_delta = *data_p;
     return;
 }
 
 void configset_walk_knee_pad_a(Config *config, void *data)
 {
-    config->walk_knee_pad_a = (float) *data;
+    float *data_p = (float *) data;
+    config->walk_knee_pad_a = *data_p;
     return;
 }
 
 void configset_walk_knee_pad_b(Config *config, void *data)
 {
-    config->walk_knee_pad_b = (float) *data;
+    float *data_p = (float *) data;
+    config->walk_knee_pad_b = *data_p;
     return;
 }
 
 void configset_servo_pins(Config *config, void *data)
 {
-    ServoPinData *servo_pin_data = (ServoPinData *) data;
-    config->servo_pins[servo_pin_data->id] = servo_pin_data->val;
+    ServoPinData *data_p = (ServoPinData *) data;
+    config->servo_pins[data_p->id] = data_p->val;
     return;
 }
 
 void configset_servo_limits(Config *config, void *data)
 {
-    ServoLimitData servo_limit_data = (ServoLimitData *) data;
-    config->servo_pins[servo_pin_data->id].min = servo_pin_data->min;
-    config->servo_pins[servo_pin_data->id].max = servo_pin_data->max;
+    ServoLimitData *data_p = (ServoLimitData *) data;
+    config->servo_pins[data_p->id].min = data_p->min;
+    config->servo_pins[data_p->id].max = data_p->max;
     return;
 }
 
