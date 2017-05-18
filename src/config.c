c4df7e255aca0cea9c0e03aa7f9caed9cbf86978
mpmumau <mpmumau@gmail.com>
2017-05-18 16:33:39 -0400

    Test

--- src/config.c
+++ src/config.c
@@ -533,7 +533,7 @@ static void config_parse_configfile(char *config_file)
 
         // config_handle_config_line(arg, val);
 
-        if (str_starts(buffer, "#"))
+        if (str_starts(buffer, "#") || str_starts(buffer, "\0"))
             continue;
 
         printf("config file buffer: %s\n", buffer);
