68f2c7fa0b3da144549542109f845e00aeb8468e
mpmumau <mpmumau@gmail.com>
2017-05-20 13:58:01 -0400

    Free last_keyfr after usage

--- src/keyframe_handler.c
+++ src/keyframe_handler.c
@@ -194,7 +194,13 @@ static void *keyhandler_main(void *arg)
             {
                 free(servo_pos);
                 servo_pos = NULL;
-            }      
+            }     
+
+            if (last_keyfr)
+            {
+                free(last_keyfr);
+                last_keyfr = NULL;
+            } 
 
             last_keyfr = (Keyframe *) list_pop(&keyframes);
         }
