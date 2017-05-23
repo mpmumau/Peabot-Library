8563fb12cf9c3d6ab116b094c2ef636ba43b3281
mpmumau <mpmumau@gmail.com>
2017-05-23 14:35:13 -0400

    Test

--- src/http_server.c
+++ src/http_server.c
@@ -112,7 +112,7 @@ static void *http_main(void *arg)
         // http_response_set(&http_response, 200, "OK", "{ 'an_object': 'set_to_this' }");
         // response_buffer = http_response_tobuffer(&http_response);
 
-        response_buffer = "HTTP/1.0 200 OK\r\nDate: Wed, Mar 22 2019 12:14:15 GMT\r\nContent-Type: application/json\r\nContent-Length:27\r\n\r\n{'an_object':'set_to_this'}";
+        response_buffer = "HTTP/1.0 200 OK\r\nDate: Wed, Mar 22 2019 12:14:15 GMT\r\nContent-Type: application/json\r\nContent-Length:27\r\n\r\n\{\'an_object\':\'set_to_this\'\}";
         printf(response_buffer);
 
         if (write(last_socket, response_buffer, 1024 * 32) < 0)
