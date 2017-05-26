73ba14b885020473b5f82c6d52180be5c01a12c6
mpmumau <mpmumau@gmail.com>
2017-05-25 21:44:37 -0400

    Test

---
+++ utils/mpu9250/mpu9250.c
@@ -0,0 +1,20 @@
+#ifndef MPU_9250_DEF
+#define MPU_9250_DEF
+
+
+/* System includes */
+#include <stdio.h>
+
+/* Raspberry Pi includes */
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+/* Header */
+#include "mpu9250.h"
+
+int main(int argc, char *argv[])
+{
+    printf("This is a test.\n");
+}
+
+#endif
---
+++ utils/mpu9250/mpu9250.h
@@ -0,0 +1,6 @@
+#ifndef MPU_9250_H_DEF
+#define MPU_9250_H_DEF
+
+
+
+#endif
