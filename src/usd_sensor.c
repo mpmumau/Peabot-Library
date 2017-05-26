c5aeaae0724cfbb47f2a1d199af8e9af1b0769a4
mpmumau <mpmumau@gmail.com>
2017-05-26 15:57:19 -0400

    dev

--- src/usd_sensor.c
+++ src/usd_sensor.c
@@ -101,7 +101,7 @@ static void *usd_sensor_main(void *arg)
 
         distance = travelTime / 58.0;
 
-        delayMicroseconds(500000);
+        delayMicroseconds(100000);
 
         printf("Distance: %12.12f\n", distance);
     }
