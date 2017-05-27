15ae67fe5f047b82b72c0b1b38c2d39054302f5e
mpmumau <mpmumau@gmail.com>
2017-05-27 01:37:47 -0400

    dev

--- src/http_server.c
+++ src/http_server.c
@@ -120,7 +120,8 @@ static void *http_main(void *arg)
         read(last_socket, http.buffer, DEFAULT_HTTP_MAX_BUFFER);
         http_request_parse(&http_request, http.buffer, sizeof(http.buffer));
 
-        write(last_socket, response_buffer, DEFAULT_HTTP_RESPONSE_SIZE);
+        if (write(last_socket, response_buffer, DEFAULT_HTTP_RESPONSE_SIZE < 0)
+            printf("errno: %d", errno);
         fsync(last_socket);
 
         close(last_socket);
