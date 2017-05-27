988a557d3ef4422b3990440d2b3ce5f251849e48
mpmumau <mpmumau@gmail.com>
2017-05-26 23:01:47 -0400

    dev

--- src/http_request.c
+++ src/http_request.c
@@ -41,8 +41,7 @@ void http_request_parse(HTTPRequest *http_request, char *raw)
     memset((void *) buffer_cpy, '\0', DEFAULT_HTTP_MAX_BUFFER);
     memcpy(raw, buffer_cpy, DEFAULT_HTTP_MAX_BUFFER);
 
-    const char delim[1];
-    delim[0] = '\n';
+    char *delim = "\n";
 
     char *line_cursor;
 
