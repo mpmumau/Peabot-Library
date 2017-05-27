79d6581ec1f04a63c33a04fb4d7039a91f6e6873
mpmumau <mpmumau@gmail.com>
2017-05-27 02:06:02 -0400

    dev

--- src/http_server.c
+++ src/http_server.c
@@ -90,8 +90,7 @@ static void *http_main(void *arg)
     listen(http.socket, DEFAULT_HTTP_MAX_CONNS);    
 
     //tmp
-    char response_buffer[DEFAULT_HTTP_RESPONSE_SIZE]; 
-    response_buffer = "HTTP/1.1 200 OK\r\nDate: Wed, May 27 2017 12:49:15 EST\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nContent-Length:30\r\n\r\n{ \"an_object\": \"set_to_this\" }\r\n\r\n";
+    char response_buffer[DEFAULT_HTTP_RESPONSE_SIZE] = "HTTP/1.1 200 OK\r\nDate: Wed, May 27 2017 12:49:15 EST\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nContent-Length:30\r\n\r\n{ \"an_object\": \"set_to_this\" }\r\n\r\n";
 
     struct timespec time, last_time;
     double tick, diff, max_tick;
