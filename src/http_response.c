0ac5a58c5e61f8a1f0d7aa2687ae01ba9c6bc082
mpmumau <mpmumau@gmail.com>
2017-05-26 22:11:23 -0400

    dev

--- src/http_response.c
+++ src/http_response.c
@@ -17,6 +17,7 @@
 #include "config_defaults.h"
 #include "main.h"
 #include "utils.h"
+#include "http_server.h"
 
 /* Header */
 #include "http_response.h"
@@ -30,44 +31,44 @@ void http_response_parse(HTTPResponse *http_response, int code, char *msg, char 
 
 char *http_response_tobuffer(HTTPResponse *http_response)
 {
-    static char output[DEFAULT_HTTP_RESPONSE_SIZE]; // limits http_responses to max 32kb
+    static char output[DEFAULT_HTTP_MAX_BUFFER]; // limits http_responses to max 256kb
 
-    for (int u = 0; u < DEFAULT_HTTP_RESPONSE_SIZE; u++)
-        output[u] = 0; 
+    // for (int u = 0; u < DEFAULT_HTTP_MAX_BUFFER; u++)
+    //     output[u] = 0; 
 
-    time_t current_time = time(NULL);
-    char time_string[64];
-    utils_mkresponsetime(current_time, time_string);
+    // time_t current_time = time(NULL);
+    // char time_string[64];
+    // utils_mkresponsetime(current_time, time_string);
 
-    char *http_line_fmt = "HTTP/1.0 %d %s\n";
-    int http_line_size = snprintf(NULL, 128, http_line_fmt, http_response->code, http_response->msg) + 1;
-    char http_line[http_line_size];
-    snprintf(http_line, http_line_size, http_line_fmt, http_response->code, http_response->msg);
+    // char *http_line_fmt = "HTTP/1.0 %d %s\n";
+    // int http_line_size = snprintf(NULL, 128, http_line_fmt, http_response->code, http_response->msg) + 1;
+    // char http_line[http_line_size];
+    // snprintf(http_line, http_line_size, http_line_fmt, http_response->code, http_response->msg);
 
-    char *date_line_fmt = "Date: %s\n";
-    int date_line_size = snprintf(NULL, 128, date_line_fmt, time_string) + 1;
-    char date_line[date_line_size];
-    snprintf(date_line, date_line_size, date_line_fmt, time_string);
+    // char *date_line_fmt = "Date: %s\n";
+    // int date_line_size = snprintf(NULL, 128, date_line_fmt, time_string) + 1;
+    // char date_line[date_line_size];
+    // snprintf(date_line, date_line_size, date_line_fmt, time_string);
 
-    char *content_type_line= "Content-Type: application/json\n";
+    // char *content_type_line= "Content-Type: application/json\n";
 
-    int max_body_size = DEFAULT_HTTP_RESPONSE_SIZE / 2;
-    int body_size = 0;
-    for ( ; body_size < max_body_size; body_size++)
-    {
-        if (http_response->body + body_size == '\0')
-            break;
-    }
-    body_size++;
+    // int max_body_size = DEFAULT_HTTP_MAX_BUFFER / 2;
+    // int body_size = 0;
+    // for ( ; body_size < max_body_size; body_size++)
+    // {
+    //     if (http_response->body + body_size == '\0')
+    //         break;
+    // }
+    // body_size++;
 
-    char *content_length_line_fmt = "Content-Length: %d\n";
-    int content_length_line_size = snprintf(NULL, 128, content_length_line_fmt, body_size);
-    char content_length_line[content_length_line_size];
-    snprintf(NULL, 128, content_length_line_fmt, body_size);
+    // char *content_length_line_fmt = "Content-Length: %d\n";
+    // int content_length_line_size = snprintf(NULL, 128, content_length_line_fmt, body_size);
+    // char content_length_line[content_length_line_size];
+    // snprintf(NULL, 128, content_length_line_fmt, body_size);
 
-    char *buffer_format = "%s%s%s%s%s%s";
-    snprintf(output, DEFAULT_HTTP_RESPONSE_SIZE, buffer_format, http_line, date_line, content_type_line, 
-        content_length_line, "\r\n", http_response->body);
+    // char *buffer_format = "%s%s%s%s%s%s";
+    // snprintf(output, DEFAULT_HTTP_RESPONSE_SIZE, buffer_format, http_line, date_line, content_type_line, 
+    //     content_length_line, "\r\n", http_response->body);
 
     return output;
 }
