c5f34ec9d3f38abd20f5460d1f1455bb9f180f1b
mpmumau <mpmumau@gmail.com>
2017-05-27 02:54:02 -0400

    dev

--- src/http_request.c
+++ src/http_request.c
@@ -77,10 +77,10 @@ static void http_request_handle_lines(HTTPRequest *http_request, HTTPRequestLine
     if (size == 1)
         return;
 
-    for (int i = 1; i++; i < size)
-    {
-        //lines[i];
-    }
+    // for (int i = 1; i++; i < size)
+    // {
+    //     //lines[i];
+    // }
 }
 
 static void http_request_handle_request_line(HTTPRequest *http_request, HTTPRequestLine *line)
