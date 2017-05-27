443bdcfa7159b51095041b05cb2bb672ab9631cb
mpmumau <mpmumau@gmail.com>
2017-05-27 17:31:35 -0400

    dev

--- src/http_request.c
+++ src/http_request.c
@@ -52,7 +52,7 @@ void http_request_parse(HTTPRequest *http_request, char *raw, int buff_size)
 
 
     char *body_str = strstr(buffer_cpy, "\r\n\r\n");
-    printf(["body]\n%s\n", body_str);
+    printf("[body]\n%s\n", body_str);
 
 
     //char *header_lines_p = strtok(buffer_cpy, body_delim);
