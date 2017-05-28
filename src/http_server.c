089011de03f978e9ee83602b5af774582f64fe66
mpmumau <mpmumau@gmail.com>
2017-05-28 02:16:44 -0400

    dev

--- src/http_server.c
+++ src/http_server.c
@@ -87,7 +87,7 @@ static void *http_main(void *arg)
     listen(http.socket, HTTP_SERVER_MAX_CONNS);    
 
     //tmp
-    char response_buffer[DEFAULT_HTTP_RESPONSE_SIZE] = "HTTP/1.1 200 OK\r\nDate: Wed, May 27 2017 12:49:15 EST\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: content-type\r\nContent-Length:30\r\n\r\n{ \"an_object\": \"set_to_this\" }\r\n\r\n";
+    //char response_buffer[DEFAULT_HTTP_RESPONSE_SIZE] = "HTTP/1.1 200 OK\r\nDate: Wed, May 27 2017 12:49:15 EST\r\nContent-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: content-type\r\nContent-Length:30\r\n\r\n{ \"an_object\": \"set_to_this\" }\r\n\r\n";
 
     while (http.running)
     {
