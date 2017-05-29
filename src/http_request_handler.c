cdbe199b84502a8250df824b636ebe4ba82d9264
mpmumau <mpmumau@gmail.com>
2017-05-29 19:23:17 -0400

    dev

--- src/http_request_handler.c
+++ src/http_request_handler.c
@@ -142,10 +142,10 @@ static void httprhnd_handle_post(HTTPRequest *http_request, HTTPResponse *http_r
     if (post_cb != NULL)
         (*post_cb)(http_request, http_response, res_data_p, (void *) req_data_p);
 
+    char *tmp = cJSON_Print(res_data_p);
+    //str_clearcopy(http_response->body, cJSON_Print(res_data_p), sizeof(http_response->body));
 
-    str_clearcopy(http_response->body, cJSON_Print(res_data_p), sizeof(http_response->body));
-
-    printf("http_response->body: \n%s\n", http_response->body);
+    printf("http_response->body: \n%s\n", tmp);
 
     cJSON_Delete(req_data_p);
     cJSON_Delete(res_data_p);
