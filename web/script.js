57c7a759fd0c3b08706b4cd606cb1899b2ab194f
mpmumau <mpmumau@gmail.com>
2017-05-29 21:18:31 -0400

    Fixed some prelim web app stuff

--- web/script.js
+++ web/script.js
@@ -1,11 +1,7 @@
-function click_button()
+function walk_button()
 {
-    var el = document.getElementById("the_button");
-    el.style.background = "red";
-
     var xml = new XMLHttpRequest();
     
-
     xml.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(JSON.parse(this.responseText)); 
@@ -13,5 +9,5 @@ function click_button()
     };
     xml.open("POST", "http://ML_DEVNET_PIBOT:9976/event/walk", true);
     xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xml.send(JSON.stringify({ cycles: 3, duration: 0.35 }));
+    xml.send(JSON.stringify({ cycles: 1, duration: 1.0 }));
 }
