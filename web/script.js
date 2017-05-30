97c28f2d32454bedeba17cad7e7be1b368efa927
mpmumau <mpmumau@gmail.com>
2017-05-29 21:22:32 -0400

    dev

--- web/script.js
+++ web/script.js
@@ -11,3 +11,45 @@ function walk_button()
     xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xml.send(JSON.stringify({ cycles: 1, duration: 1.0 }));
 }
+
+function turn_button()
+{
+    var xml = new XMLHttpRequest();
+    
+    xml.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText)); 
+        }
+    };
+    xml.open("POST", "http://ML_DEVNET_PIBOT:9976/event/turn", true);
+    xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xml.send(JSON.stringify({ cycles: 30, duration: 0.35 }));
+}
+
+function elevate_button()
+{
+    var xml = new XMLHttpRequest();
+    
+    xml.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText)); 
+        }
+    };
+    xml.open("POST", "http://ML_DEVNET_PIBOT:9976/event/elevate", true);
+    xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xml.send(JSON.stringify({ reverse: true, duration: 3.0 }));
+}
+
+function extend_button()
+{
+    var xml = new XMLHttpRequest();
+    
+    xml.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText)); 
+        }
+    };
+    xml.open("POST", "http://ML_DEVNET_PIBOT:9976/event/extend", true);
+    xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xml.send(JSON.stringify({ reverse: true, duration: 3.0 }));
+}
