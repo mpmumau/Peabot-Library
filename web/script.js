7e2ca1abd73eb4883d10d20da40055ea6ba377cb
mpmumau <mpmumau@gmail.com>
2017-05-30 17:13:33 -0400

    dev

--- web/script.js
+++ web/script.js
@@ -53,3 +53,17 @@ function extend_button()
     xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xml.send(JSON.stringify({ reverse: true, duration: 3.0 }));
 }
+
+function reset_button()
+{
+    var xml = new XMLHttpRequest();
+    
+    xml.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText)); 
+        }
+    };
+    xml.open("POST", "http://ML_DEVNET_PIBOT:9976/event/reset", true);
+    xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xml.send();
+}
