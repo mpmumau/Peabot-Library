0cc4e5173ca2ff971b4a5d961989e14bb30d5ab9
mpmumau <mpmumau@gmail.com>
2017-05-30 15:55:26 -0400

    dev

--- src/http_server.c
+++ src/http_server.c
@@ -96,8 +96,8 @@ static void *http_main(void *arg)
     fd_set socket_fd_set;
     int socket_select_result, iof = -1;
     struct timeval timeout;    
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 25000; 
+    timeout.tv_sec = 3;
+    timeout.tv_usec = 0; 
 
     FD_ZERO(&socket_fd_set);
     FD_SET(http.socket, &socket_fd_set);   
@@ -108,7 +108,10 @@ static void *http_main(void *arg)
     {
         socket_select_result = select(http.socket + 1, &socket_fd_set, NULL, NULL, &timeout);
         if (socket_select_result < 0)
+        {
+            printf("timeout\n");
             continue;
+        }
         else if (socket_select_result > 0 && FD_ISSET(http.socket, &socket_fd_set))
         {
           if ((iof = fcntl(http.socket, F_GETFL, 0)) != -1)
