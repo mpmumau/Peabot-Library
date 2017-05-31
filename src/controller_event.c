2af969ceebbb6c6525a62e14ca30f03970e9be77
mpmumau <mpmumau@gmail.com>
2017-05-30 22:17:36 -0400

    dev

--- src/controller_event.c
+++ src/controller_event.c
@@ -53,7 +53,7 @@ bool cntlevent_walk(MVCData *mvc_data)
 
     printf("event_walk_data->cycles: %d\n", event_walk_data->cycles);
     printf("event_walk_data->duration: %f\n", event_walk_data->duration);
-    printf("event_walk_data->reverse: %d\n", event_walk_data->reverse);
+    printf("event_walk_data->reverse: %s\n", event_walk_data->reverse ? "true" : "false");
 
     event_add(EVENT_WALK, (void *) event_walk_data);
     return true;
