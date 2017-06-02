57b9fef77caef06b73e501bf6d74944dcc6859c7
mpmumau <mpmumau@gmail.com>
2017-06-02 18:03:47 -0400

    dev

--- src/event_callbacks.c
+++ src/event_callbacks.c
@@ -131,11 +131,13 @@ void eventcb_strafe(void *arg)
 {
     EventStrafeData *strafe_data = (EventStrafeData *) arg;
 
+    printf("got here\n");
+
     unsigned short cycles = strafe_data->cycles;
     double duration = strafe_data->duration;
     bool reverse = strafe_data->reverse;
 
-    printf("got here\n");
+
    
     double *duration_p;
 
