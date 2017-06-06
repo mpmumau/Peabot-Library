8ad21d5adfac4906564aa3dcfc571081dd90980b
mpmumau <mpmumau@gmail.com>
2017-06-06 15:03:53 -0400

    ev

--- src/event_callbacks.c
+++ src/event_callbacks.c
@@ -98,8 +98,6 @@ void eventcb_walk(void *arg)
         keyhandler_add(KEYFR_WALK, (void *) duration_p, reverse, i > 0);
     }
 
-    keyhandler_add(KEYFR_ELEVATE, (void *) NULL, false, false);
-
     eventcb_logcb("Added KEYFR_WALK keyframes.");
 }
 
@@ -148,7 +146,6 @@ void eventcb_strafe(void *arg)
         keyhandler_add(KEYFR_STRAFE, (void *) duration_p, reverse, i > 0);
     }
 
-    keyhandler_add(KEYFR_ELEVATE, (void *) NULL, false, false);
     eventcb_logcb("Added KEYFR_STRAFE keyframes.");
 }
 
--- web/src/components/gamepad-component.js
+++ web/src/components/gamepad-component.js
@@ -9,45 +9,70 @@ class GamepadComponent extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            is_moving: false
-        };
+
+        this.is_moving = false;
+        this.timeout_obj = undefined;
+        this.timeout_milli = 1000;
+        this.robot_url = "http://ML_DEVNET_PIBOT:9976/";
+
+        this.getMvmtVals = this.getMvmtVals.bind(this);
+        this.sendMvmtReq = this.sendMvmtReq.bind(this);
+        this.moveRobot = this.moveRobot.bind(this);
+        this.stopMoving = this.stopMoving.bind(this);
+        this.haltRobot = this.haltRobot.bind(this);
     }
 
-    moveRobot(mvmt_type) {
-        var mvmt_name;
-        var reverse;
+    getMvmtVals(mvmt_type) {
+        var obj = null;
 
-        switch (mvmt_type) {
-            case "up":
-                mvmt_name = "walk"
-                reverse = false;
-                break;
-            case "down":
-                reverse = true;
-                mvmt_name = "walk"
-                break;
-            case "left":
-                reverse = false;
-                mvmt_name = "strafe"
-                break;
-            case "right":
-                reverse = true;
-                mvmt_name = "strafe"
-                break;
-        }
-        fetch('http://ML_DEVNET_PIBOT:9976/event/' + mvmt_name, {
+        if (mvmt_type == "up" || mvmt_type == "down" || 
+            mvmt_type == "left" || mvmt_type == "right")
+            obj = {};
+        else
+            return obj;
+
+        if (mvmt_type == "up" || mvmt_type == "down")
+            obj.mvmt_name = "walk";
+        else
+            obj.mvmt_name = "strafe";
+
+        if (mvmt_type == "up" || mvmt_type == "left")
+            obj.reverse = false;
+        else
+            obj.revrese = true;
+
+        return obj;
+    }
+
+    sendMvmtReq(mvmt_type) {
+        var mvmt_data = this.getMvmtVals(mvmt_type);
+
+        console.log(mvmt_data);
+
+        var url = this.robot_url + "event/" + mvmt_data.mvmt_name;
+
+        fetch(url, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                cycles: 5,
-                duration: 0.75,
-                reverse: reverse
+                cycles: 1,
+                duration: (this.timeout_milli / 1000),
+                reverse: mvmt_data.reverse
             })
         });
+    }
+
+    moveRobot(mvmt_type) {
+        var mvmt_vals = this.getMvmtVals(mvmt_type);
+        this.sendMvmtReq(mvmt_type);
+        this.timeout_obj = setTimeout(() => this.moveRobot(mvmt_type), this.timeout_milli);
+    }
+
+    stopMoving() {
+        var t = clearTimeout(this.timeout_obj);
     }
 
     haltRobot() {
@@ -60,23 +85,37 @@ class GamepadComponent extends Component {
         return (
                 <section className="gamepad-component">
                     <div className="d-pad">
-                        <button className='up' onClick={() => this.moveRobot("up")}>
+                        <button className='up' 
+                            onMouseEnter={() => this.moveRobot("up")} 
+                            onMouseLeave={() => this.stopMoving()}
+                        >
                             <span className="oi mega" data-glyph="arrow-thick-top"></span>
                         </button>
 
-                        <button className='stop' onClick={() => this.haltRobot()}>
+                        <button className='stop' 
+                            onMouseEnter={() => this.haltRobot()} 
+                        >
                             <span className="oi mega" data-glyph="target"></span>
                         </button>
 
-                        <button className='down' onClick={() => this.moveRobot("down")}>
+                        <button className='down' 
+                            onMouseEnter={() => this.moveRobot("down")} 
+                            onMouseLeave={() => this.stopMoving()}
+                        >
                             <span className="oi mega" data-glyph="arrow-thick-bottom"></span>
                         </button>
 
-                        <button className='right' onClick={() => this.moveRobot("right")}>
+                        <button className='right' 
+                            onMouseEnter={() => this.moveRobot("right")} 
+                            onMouseLeave={() => this.stopMoving()}
+                        >
                             <span className="oi mega" data-glyph="arrow-thick-right"></span>
                         </button>
 
-                        <button className='left' onClick={() => this.moveRobot("left")}>
+                        <button className='left' 
+                            onMouseEnter={() => this.moveRobot("left")} 
+                            onMouseLeave={() => this.stopMoving()}
+                        >
                             <span className="oi mega" data-glyph="arrow-thick-left"></span>
                         </button>
                     </div>
