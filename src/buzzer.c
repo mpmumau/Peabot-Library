b9343d3fef2b4f3f6b170ab55328423e3015c44c
mpmumau <mpmumau@gmail.com>
2017-06-10 18:07:01 -0400

    dev

--- src/buzzer.c
+++ src/buzzer.c
@@ -91,8 +91,8 @@ void *buzzer_main(void *arg) {
         tick = 0.0;
         flipped = !flipped;
 
-        pinMode(buzzer_pin_a, flipped ? OUTPUT : INPUT);
-        pinMode(buzzer_pin_b, flipped ? INPUT : OUTPUT);
+        pinMode(buzzer_pin_a, flipped ? INPUT : OUTPUT);
+        pinMode(buzzer_pin_b, flipped ? OUTPUT : INPUT);
 
         digitalWrite(buzzer_pin_a, flipped ? HIGH : LOW);
         digitalWrite(buzzer_pin_b, flipped ? LOW : HIGH);
