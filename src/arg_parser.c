4bd46bb65b7c08f6bfad3c4971333fb0e8c65d7c
Matt Mumau <mpmumau@gmail.com>
2020-07-26 18:41:20 -0400

    Testing getopt_long

--- src/arg_parser.c
+++ src/arg_parser.c
@@ -5,11 +5,52 @@
 
 /* Standard library includes */
 #include <stdio.h>
+#include <getopt.h>
+
+static int test_flag;
 
 /* Header */
 #include "arg_parser.h"
 
 void argparser_parse(int argc, char *argv[])
 {
-    printf("We rocking now!\n");
+    int c;
+
+    while (1) {
+        static struct option long_options[] = 
+        {
+            {"mumau", no_argument, &test_flag, 1}
+        }
+
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "abc:d:f:", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+            case 0:
+                if (long_options[option_index].flag != 0)
+                    break;
+
+                printf ("option %s", long_options[option_index].name);
+                if (optarg)
+                    printf (" with arg %s", optarg);
+                printf ("\n");
+                break;
+
+            case 'a':
+                puts("option -a\n");
+                break;
+
+            default:
+                abort();
+        }
+    }
+
+    if (test_flag)
+        puts("verbose flag is set");
+
 }
